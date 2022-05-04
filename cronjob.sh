af79d2e265ecd96c9a810b43dbd265f5fe01becf
Vekoze <vekozepro@gmail.com>
2022-05-04 18:07:23 +0200

    script that creates cronjobs

---
+++ cronjob.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# ': crontal -l' gets all cronjobs but doesn't print 'no crontab for <user' if empty
+( : crontab -l ; echo "*/5 * * * * $HOME/parsing.sh") | crontab -
+
+( crontab -l ; echo "0 * * * * $HOME/generate_pdf.sh") | crontab -
