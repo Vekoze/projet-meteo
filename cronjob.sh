0a6b1e62404c0a5df1aed834385c46663ee7e92e
Vekoze <vekozepro@gmail.com>
2022-05-31 11:43:24 +0200

    rename script

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# ': crontal -l' gets all cronjobs but doesn't print 'no crontab for <user' if empty
-( : crontab -l ; echo "*/5 * * * * $HOME/parsing.sh") | crontab -
+# ': crontal -l' gets all cronjobs but doesn't print 'no crontab for <user>' if empty
+( : crontab -l ; echo "*/5 * * * * $HOME/project.sh") | crontab -
 ( crontab -l ; echo "0 * * * * $HOME/generate_pdf.sh") | crontab -
