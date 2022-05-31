c2ccd9f543c6f2769cf0fd21e0b0b87527015f97
Vekoze <vekozepro@gmail.com>
2022-05-31 21:04:15 +0200

    file's name edited

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# ': crontal -l' gets all cronjobs but doesn't print 'no crontab for <user>' if empty
 ( : crontab -l ; echo "*/5 * * * * $HOME/project.sh") | crontab -
-( crontab -l ; echo "0 * * * * $HOME/generate_pdf.sh") | crontab -
+( crontab -l ; echo "59 23 * * * $HOME/generate_report.sh") | crontab -
