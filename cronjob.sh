915aa333196c4ea1a65eea1b1bd0396ded867a3c
Vekoze <vekozepro@gmail.com>
2022-06-01 12:02:00 +0200

    edit shell script path

--- cronjob.sh
+++ cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-( : crontab -l ; echo "*/5 * * * * $HOME/project.sh") | crontab -
-( crontab -l ; echo "59 23 * * * $HOME/generate_report.sh") | crontab -
+( : crontab -l ; echo "*/5 * * * * ./project.sh") | crontab -
+( crontab -l ; echo "59 23 * * * ./generate_report.sh") | crontab -
