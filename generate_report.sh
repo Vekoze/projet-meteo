54de6714fa78b3697850cb457638d1d6df4c32f4
Vekoze <vekozepro@gmail.com>
2022-06-01 18:09:41 +0200

    fix axis & remove data file

--- generate_report.sh
+++ generate_report.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-readonly WEATHER_PATH="$HOME/weather.json"
-
 get_json_value(){
     local key=$1
-    awk -v path=$key -f json.awk $WEATHER_PATH
+    awk -v path=$key -f json.awk weather.json
 }
 
-float_to_int(){
-    printf "%.0f" $1
+round_upper(){
+    echo "$1+1" | bc
 }
 
+export LC_NUMERIC=en_US.utf-8
 temperature_min=`awk 'BEGIN{x=99} {if($2<x) x=$2} END{print x}' temperature`
-temperature_max=`awk 'BEGIN{x=0} {if($2>x) x=$2} END{print x}' temperature`
+temperature_max=`awk 'BEGIN{max=0} { if(max < $2) max = $2 } END{print max}' temperature`
 humidity_min=`awk 'BEGIN{x=99} {if($2<x) x=$2} END{print x}' humidity`
 humidity_max=`awk 'BEGIN{x=0} {if($2>x) x=$2} END{print x}' humidity`
 global=`awk 'BEGIN{x=0} {arr[$2]++} {for(key in arr) if(x<arr[key]) k=key; x=arr[key]} END{print k}' sky`
@@ -22,7 +21,6 @@ lon=$(get_json_value "coord.lon")
 lat=$(get_json_value "coord.lat")
 date=`date +%d-%m-%Y`
 
-mkdir -p graph
 cat <<EOT >> temperature.p
 set title 'temperatures'
 set terminal png size 1000,350
@@ -30,12 +28,11 @@ set xdata time
 set format x '%H:%M'
 set timefmt '%H:%M'
 set xrange ['00:00':*]
-set yrange [$(float_to_int $temperature_min):$(float_to_int $temperature_max)]
+set yrange [$(round_upper $temperature_min):$(round_upper $temperature_max)]
 set grid
-set ytics 0.5
 set xtics rotate
 set key off
-set output 'graph/temperature.png'
+set output 'graph_temperature.png'
 plot 'temperature' using 1:2 with lines
 EOT
 
@@ -46,32 +43,26 @@ set xdata time
 set format x '%H:%M'
 set timefmt '%H:%M'
 set xrange ['00:00':*]
-set yrange [$(float_to_int $humidity_min):$(float_to_int $humidity_max)]
+set yrange [$(round_upper $humidity_min):$(round_upper $humidity_max)]
 set grid
-set ytics 0.5
 set xtics rotate
 set key off
-set output 'graph/humidity.png'
+set output 'graph_humidity.png'
 plot 'humidity' using 1:2 with lines
 EOT
 
 cat <<EOT >> sky.p
 set terminal png size 1300,150
-set output 'graph/sky.png'
-
+set output 'graph_sky.png'
 set xtics font ", 8"
 unset ytics
-
 set xdata time
 set format x '%H:%M'
 set timefmt '%H:%M'
-
 set xtics ()
 set for [i=0:24:2] xtics add (sprintf("%3.0f:00", i%24) i*100)
-
 set xrange [0:2400]
 set yrange [0:100]
-
 set macros
 begin="binary filetype=png"
 end="dx=1 dy=0.3 with rgbalpha notitle"
@@ -113,26 +104,26 @@ header-includes: |
 
 ## Ciel
 
-![](graph/sky.png "Ciel")
+![](graph_sky.png "Ciel")
 
 - **global**: $global
 
 ## Température
 
-![](graph/temperature.png "Temperature")
+![](graph_temperature.png "Temperature")
 
 - **température min**: $temperature_min
 - **température max**: $temperature_max
 
 ## Humidité
 
-![](graph/humidity.png "Humidite")
+![](graph_humidity.png "Humidite")
 
 - **humidité min**: $humidity_min
 - **humidité max**: $humidity_max
 EOT
 
-pandoc -V geometry:margin=1.2in -s -o report.pdf report.md
+pandoc -V geometry:margin=1.2in -s -o rapport_meteo_enjalbert_$date.pdf report.md
 
 # Cleanup
 
@@ -142,9 +133,10 @@ rm -f sky.p
 
 rm -f report.md
 
-#rm -f temperature
-#rm -f humidity
-#rm -f sky
+rm -f temperature
+rm -f humidity
+rm -f sky
 
-#rm graph/*.png
-#rmdir graph
+rm -f graph_temperature.png
+rm -f graph_humidity.png
+rm -f graph_sky.png
